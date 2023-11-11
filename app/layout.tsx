241dc5157f0eeef57a9de0c4f44915448e43e855
Zaheer khan <83355375+Zaheer-zk@users.noreply.github.com>
2023-11-12 00:00:38 +0530

    add layout and assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,30 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import React from 'react';
+import { Inter, Space_Grotesk } from 'next/font/google';
+import type { Metadata } from 'next';
+
+import './globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk',
+});
+
+export const metadata: Metadata = {
+  title: 'Islamic-overflow',
+  description:
+    'A comprehensive online platform dedicated to sharing knowledge, resources, and discussions on Islamic teachings, history, and culture. Engage with a community of scholars, students, and enthusiasts to explore the rich heritage of Islam.',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,9 +32,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover: text-primary-500',
+        },
+      }}
+    >
       <html lang='en'>
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );

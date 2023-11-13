efd9433c5ee87f80afe4dc7674350cfb78f67fc1
Zaheer khan <83355375+Zaheer-zk@users.noreply.github.com>
2023-11-13 18:51:56 +0530

    Add search and filters

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,36 @@
+import HomeFilters from '@/components/home/HomeFilters';
+import Filters from '@/components/shared/search/Filters';
+import LocalSearchBar from '@/components/shared/search/LocalSearchBar';
+import { Button } from '@/components/ui/button';
+import { HomePageFilters } from '@/constants/filters';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <div className='flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center'>
+        <h1 className='h1-bold text-dark100_light900'>All Questions</h1>
+        <Link href='/ask-question' className='flex justify-end max-sm:w-full'>
+          <Button className='primary-gradient inline-flex h-9 min-h-[46px] items-center justify-center rounded-md bg-slate-900 px-4 py-3 text-sm font-medium  shadow transition-colors hover:bg-slate-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90 dark:focus-visible:ring-slate-300'>
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+      <div className='mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center'>
+        <LocalSearchBar 
+         route='/'
+         iconPosition='left'
+         imgSrc='/assets/icons/search.svg'
+         placeholder='Search for questions...'
+         otherClasses='flex-1'/>
+        <Filters
+          filters={HomePageFilters}
+          otherClasses='min-h-[56px] sm:min-w-[170px]'
+          containerClasses='hidden max-md:flex'
+        />
+      </div>
+
+      <HomeFilters />
+    </>
+  );
 }
---
+++ components/shared/search/LocalSearchBar.tsx
@@ -0,0 +1,56 @@
+'use client';
+import { Input } from '@/components/ui/input';
+import Image from 'next/image';
+import React from 'react';
+
+interface Props {
+  route: string;
+  iconPosition: string;
+  imgSrc: string;
+  placeholder: string;
+  otherClasses: string;
+}
+
+const LocalSearchBar = ({
+  route,
+  iconPosition,
+  imgSrc,
+  placeholder,
+  otherClasses,
+}: Props) => {
+  return (
+    <div
+      className={`background-light800_darkgradient flex min-h-[56px] flex-1 grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
+    >
+      {iconPosition === 'left' && (
+        <Image
+          src={imgSrc}
+          width={24}
+          height={24}
+          alt='search icon'
+          className='cursor-pointer text-transparent'
+        />
+      )}
+      <Input
+        type='text'
+        placeholder={placeholder}
+        className='paragraph-regular no-focus placeholder
+        background-light800_darkgradient border-none shadow-none outline-none'
+        value=''
+        onChange={() => {}}
+      />
+
+      {iconPosition === 'right' && (
+        <Image
+          src={imgSrc}
+          width={24}
+          height={24}
+          alt='search icon'
+          className='cursor-pointer text-transparent'
+        />
+      )}
+    </div>
+  );
+};
+
+export default LocalSearchBar;
